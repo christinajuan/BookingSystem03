d94da0665cb8f9f45a949093323812d0c258f30b
christinajuan <christjuam1104@gmail.com>
2016-12-28 11:17:08 +0800

    2016/12/28資料庫結合

---
+++ app/src/main/java/com/example/user/ordersystem/MainActivity.java
@@ -0,0 +1,224 @@
+package com.example.user.ordersystem;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener
+        {
+
+            private LoginButton loginButton;
+            private CallbackManager callbackManager;
+            LoginManager lmg;
+            private ProfileTracker profileTracker;
+            private String loginID;
+            private int count;
+
+
+            
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        Button button = (Button)findViewById(R.id.login);
+        button.setOnClickListener(clickListener);
+
+
+        //-----------------FB--------------------
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+
+
+
+
+        lmg.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                Log.v("ppking" , "oldProfile : "+ oldProfile );
+                Log.v("ppking" , "currentProfile : "+ currentProfile );
+
+                if(currentProfile !=null) {
+                    loginID = currentProfile.getId();
+                    //TODO : loginID 是由FB讀取出來判斷帳戶的依據並儲存於資料庫內
+                    new Thread() {
+                        @Override
+                        public void run() {
+                            try {
+                                count++;
+
+                                MultipartUtility mu = new MultipartUtility("https://android-test-db-ppking2897.c9users.io/DataBase/Insert02.php", "UTF-8");
+                                if(count<2) {
+                                    mu.addFormField("accountId", loginID);
+                                    mu.addFormField("seatIdNumber", "0");
+                                    mu.addFormField("checkOut", "0");
+                                    List<String> ret = mu.finish();
+                                    Log.v("ppking","ret" + ret);
+                                    Log.v("ppking","profileTracker" + loginID);
+                                    Intent it = new Intent(MainActivity.this,MyService.class);
+                                    it.putExtra("ppking" ,loginID);
+                                    startService(it);
+                                }
+                            } catch (Exception e) {
+                                Log.v("ppking", "DB Error:" + e.toString());
+                            }
+                        }
+                    }.start();
+                }
+            }
+
+        };
+        //-----------------end-----------------------------------
+    }
+    //-------------FB-------------------------
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+        //Log.v("ppking" , "onActivityResult");
+    }
+    //--------------end---------------------------
+
+
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            Intent it = new Intent();
+            it.setClass(MainActivity.this,SeatPage.class);
+            startActivity(it);
+            finish();
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    View.OnClickListener clickListener = new View.OnClickListener(){
+        public void onClick(View v){
+            Intent it = new Intent();
+            it.setClass(MainActivity.this,SeatPage.class);
+            startActivity(it);
+            finish();
+        }
+    };
+
+}
---
+++ app/src/main/java/com/example/user/ordersystem/SeatPage.java
@@ -0,0 +1,283 @@
+package com.example.user.ordersystem;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SeatPage extends AppCompatActivity {
+    Button[] btns = new Button[10];
+    private Button doSetDate;
+    private Button doSetTime;
+    private TextView textDate;
+    private TextView textTime;
+    private DatePickerDialog datePickerDialog;
+    private TimePickerDialog timePickerDialog;
+
+    private TextView tableNmb;
+    private String tableNumber;
+
+    //----------------------------------
+    private int count;
+    private UIHandler uiHandler;
+    private String SeatState1 , SeatState2 , SeatState3 , SeatState4 , SeatState5 , SeatState6,
+                   SeatState7 , SeatState8 , SeatState9 , SeatState0;
+    private String loginId;
+    private MyReceiver myReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seat_page);
+
+        doFindView();
+        GregorianCalendar calendar = new GregorianCalendar();
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                int month = monthOfYear+1;
+                textDate.setText((year+"/"+month+"/"+dayOfMonth));
+            }
+        },calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH));
+        timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener(){
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                textTime.setText((hourOfDay>12?hourOfDay-12:hourOfDay)
+                +":"+minute+" "+(hourOfDay>12?"PM":"AM"));
+
+            }
+        },calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),false);
+
+        Button button = (Button)findViewById(R.id.goSuccessPage);
+        button.setOnClickListener(clickListenerGo);
+
+        for (int i = 0; i < 10; i++) {
+            btns[i].setOnClickListener(clickListener);}
+
+        //---------DATA BASE-----------
+        pullData();
+        uiHandler = new UIHandler();
+        //---------從Service提取資料---------
+        myReceiver = new MyReceiver();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("test");
+        registerReceiver(myReceiver,intentFilter);
+
+
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Intent it = new Intent(SeatPage.this,MyService.class);
+        startService(it);
+    }
+
+    //找View By ID
+    public void doFindView(){
+
+
+        btns[0] = (Button)findViewById(R.id.b0);
+        btns[1] = (Button)findViewById(R.id.b1);
+        btns[2] = (Button)findViewById(R.id.b2);
+        btns[3] = (Button)findViewById(R.id.b3);
+        btns[4] = (Button)findViewById(R.id.b4);
+        btns[5] = (Button)findViewById(R.id.b5);
+        btns[6] = (Button)findViewById(R.id.b6);
+        btns[7] = (Button)findViewById(R.id.b7);
+        btns[8] = (Button)findViewById(R.id.b8);
+        btns[9] = (Button)findViewById(R.id.b9);
+
+
+        doSetDate = (Button)findViewById(R.id.buttonDate);
+        doSetTime = (Button)findViewById(R.id.buttonTime);
+        textDate = (TextView)findViewById(R.id.datetext);
+        textTime = (TextView)findViewById(R.id.timetext);
+        tableNmb = (TextView)findViewById(R.id.tableNmb);
+    }
+
+    //日期視窗
+    public void setDate(View v){
+        datePickerDialog.show();
+    }
+    //日期視窗
+    public void setTime(View v){
+        timePickerDialog.show();
+    }
+
+
+    //位置按鈕
+    View.OnClickListener clickListener = new View.OnClickListener() {
+
+        public void onClick(View v) {
+            for(int i = 0;i<10;i++) {
+                btns[i].setBackgroundColor(getResources().getColor(R.color.colorWhite));
+            }
+            v.setBackgroundColor(getResources().getColor(R.color.colorOrange));
+            //顯示桌號 tableNmb
+            Button b = (Button) v;
+            tableNumber = b.getText().toString();
+            tableNmb.setText("你選擇了 " + tableNumber + "號桌");
+
+        }
+    };
+
+
+    //移動按鈕&上傳資料
+    View.OnClickListener clickListenerGo = new View.OnClickListener(){
+        public void onClick(View v){
+            Intent it = new Intent();
+            it.setClass(SeatPage.this,SuccessPage.class);
+            pushData();
+            startActivity(it);
+            finish();
+
+            //上傳資料
+
+        }
+    };
+
+    //
+    //桌號資料變數> tableNumber
+
+
+
+
+
+
+
+
+//---------------------DATA BASE-----------------------
+
+    //提取資料
+    public void pullData(){
+
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+
+                    MultipartUtility mu = new MultipartUtility("https://android-test-db-ppking2897.c9users.io/DataBase/SeatQuery02.php", "UTF-8");
+                    List<String> ret = mu.finish();
+
+                    parseJSON(ret.toString());
+
+
+                } catch (Exception e) {
+                    Log.v("ppking", "DB Error:" + e.toString());
+                }
+            }
+        }.start();
+    }
+    public void pushData(){
+
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    count++;
+
+                    MultipartUtility mu = new MultipartUtility("https://android-test-db-ppking2897.c9users.io/DataBase/AccountUpload02.php", "UTF-8");
+                    if(count<2) {
+                        mu.addFormField("accountid", loginId);
+                        mu.addFormField("resdate", textDate.getText().toString());
+                        mu.addFormField("restime", textTime.getText().toString());
+                        mu.addFormField("seatidnumber",tableNumber);
+                        List<String> ret = mu.finish();
+                        Log.v("ppking", "ret update:: " + ret);
+                    }
+                } catch (Exception e) {
+                    Log.v("ppking", "DB Error:" + e.toString());
+                }
+            }
+        }.start();
+    }
+
+
+
+    //接收JSON格式並且找特定字串
+    private void parseJSON(String json){
+        LinkedList accountInfo = new LinkedList<>();
+        try{
+
+//            JSONObject jsonObject = new JSONArray(json).getJSONObject(0);
+            JSONArray jsonArray = new JSONArray(json).getJSONArray(0);
+            for(int i = 0 ; i <=9 ; i++ ) {
+                String stringNo = jsonArray.getString(i);
+                accountInfo.add(stringNo);
+//                String stringNo1 = jsonArray.getString(1);
+//                accountInfo.add(stringNo1);
+            }
+
+            Message mesg = new Message();
+            Bundle data = new Bundle();
+            for (int i = 0 ; i<=9 ; i++) {
+                data.putCharSequence("data"+i, accountInfo.get(i).toString());
+//            data.putCharSequence("data1", accountInfo.get(1).toString());
+//            data.putCharSequence("data2",accountInfo.get(2).toString());
+            }
+            mesg.setData(data);
+            mesg.what=0;
+            uiHandler.sendMessage(mesg);
+
+
+        }catch (Exception e){
+            Log.v("ppking", "Error : " + e.toString());
+        }
+    }
+    //由handler這邊將資料丟到前景UI
+    private class UIHandler extends android.os.Handler{
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case 0 :
+//                    textView.setText("Accound : "+msg.getData().getCharSequence("data0")+"\n");
+//                    textView.append("SeatIdNumber : "+msg.getData().getCharSequence("data1")+"\n");
+//                    textView.append("Checkout : $"+msg.getData().getCharSequence("data2")+"\n");
+                    //TODO:各座位的狀態，去判斷座位
+                    SeatState0 = msg.getData().getCharSequence("data0").toString();
+                    SeatState1 = msg.getData().getCharSequence("data1").toString();
+                    SeatState2 = msg.getData().getCharSequence("data2").toString();
+                    SeatState3 = msg.getData().getCharSequence("data3").toString();
+                    SeatState4 = msg.getData().getCharSequence("data4").toString();
+                    SeatState5 = msg.getData().getCharSequence("data5").toString();
+                    SeatState6 = msg.getData().getCharSequence("data6").toString();
+                    SeatState7 = msg.getData().getCharSequence("data7").toString();
+                    SeatState8 = msg.getData().getCharSequence("data8").toString();
+                    SeatState9 = msg.getData().getCharSequence("data9").toString();
+                    break;
+            }
+        }
+    }
+    private class MyReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //-----------從Service提取資料
+            loginId = intent.getStringExtra("ppking");
+            Log.v("ppking" , "loginId" + loginId);
+        }
+    }
+
+    //------------------------------END----------------------------------------------------------
+}
